4fee30024ba4e65c089dafea907595629b1c4664
khanhvq <khanhvq@vitalify.asia>
2024-05-30 10:39:32 +0700

    add auto scale and load balance

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ from infra import VpcNetWorkStack, Ec2InstanceStack
 
 app = cdk.App()
 vpc_stack = VpcNetWorkStack(app,'VpcNetworkStack')
-ec2_stack = Ec2InstanceStack(app,'InstanceStack')
-ec2_stack.create_ec2(vpc_stack.vpc, 'web instance')
+ec2_stack = Ec2InstanceStack(app,'InstanceStack', vpc_stack.vpc)
+ec2_web = ec2_stack.create_load_balance('web instance','infra/scripts/web_start.sh')
+ec2_admin = ec2_stack.create_ec2('admin instance')
 ec2_stack.add_dependency(vpc_stack)
 app.synth()
--- infra/e2c_instance.py
+++ infra/e2c_instance.py
@@ -1,12 +1,15 @@
 from aws_cdk import (
-    Stack,
+    App, CfnOutput, Stack,
     aws_ec2 as ec2,
     aws_iam as iam,
+    aws_autoscaling as autoscaling,
+    aws_elasticloadbalancingv2 as elbv2,
 )
 from constructs import Construct
 class Ec2InstanceStack(Stack):
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+    def __init__(self, scope: Construct, construct_id: str,vpc, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
+        self.vpc = vpc
         if not hasattr(self, 'amzn_linux'):
             #AMI
             self.amzn_linux = ec2.MachineImage.latest_amazon_linux(
@@ -20,14 +23,68 @@ class Ec2InstanceStack(Stack):
             self.role = iam.Role(self, "InstanceSSM", assumed_by=iam.ServicePrincipal("ec2.amazonaws.com"))
             self.role.add_managed_policy(iam.ManagedPolicy.from_aws_managed_policy_name("AmazonSSMManagedInstanceCore"))
         
-    def create_ec2(self, vpc, name):
+    def create_ec2(self, name):
         # Instance
-        instance = ec2.Instance(
-            self,
-            name,
-            instance_type=ec2.InstanceType("t2.micro"),
-            machine_image=ec2.MachineImage.latest_amazon_linux2023(),
-            vpc=vpc,
-            associate_public_ip_address=False,
-        )
+        instance = ec2.Instance(self, name,
+            instance_type=ec2.InstanceType("t3.nano"),
+            machine_image=self.amzn_linux,
+            vpc = self.vpc,
+            role = self.role
+            )
         return instance
+    
+    def create_auto_scale(self, name, sh_path, load_balancer_sg):
+        # Security Group for Web Instance
+        web_instance_sg = ec2.SecurityGroup(self, "InstanceSG " + name,
+                                            vpc=self.vpc,
+                                            description="Allow HTTP access only from the Load Balancer",
+                                            allow_all_outbound=True
+                                            )
+
+        web_instance_sg.add_ingress_rule(load_balancer_sg, ec2.Port.tcp(80), "Allow HTTP from Load Balancer")
+        # Set user data
+        data = open(sh_path, "rb").read()
+        user_data=ec2.UserData.for_linux()
+        user_data.add_commands(str(data,'utf-8'))
+
+        asg = autoscaling.AutoScalingGroup(
+            self,
+            "ASG " + name,
+            vpc = self.vpc,
+            instance_type=ec2.InstanceType.of(
+                ec2.InstanceClass.BURSTABLE2,
+                ec2.InstanceSize.MICRO
+            ),
+            machine_image=self.amzn_linux,
+            user_data=user_data,
+            role = self.role,
+            max_capacity = 1,
+            security_group=web_instance_sg
+
+        )
+        return asg
+    
+    def create_load_balance(self, name, sh_path):
+        # Security Group for Load Balancer
+        load_balancer_sg = ec2.SecurityGroup(self, "LoadBalancerSG " + name,
+                                             vpc=self.vpc,
+                                             description="Allow HTTP and HTTPS access from anywhere",
+                                             allow_all_outbound=True
+                                             )
+        load_balancer_sg.add_ingress_rule(ec2.Peer.any_ipv4(), ec2.Port.tcp(80), "Allow HTTP from anywhere")
+        load_balancer_sg.add_ingress_rule(ec2.Peer.any_ipv6(), ec2.Port.tcp(80), "Allow HTTP from anywhere (IPv6)")
+        load_balancer_sg.add_ingress_rule(ec2.Peer.any_ipv4(), ec2.Port.tcp(443), "Allow HTTPS from anywhere")
+        load_balancer_sg.add_ingress_rule(ec2.Peer.any_ipv6(), ec2.Port.tcp(443), "Allow HTTPS from anywhere (IPv6)")
+        # Load balancer
+        lb = elbv2.ApplicationLoadBalancer(
+            self,
+            "LoadBalancer " + name,
+            vpc=self.vpc,
+            security_group=load_balancer_sg,
+            internet_facing=True)
+
+        asg = self.create_auto_scale(name, sh_path, load_balancer_sg)
+        listener = lb.add_listener("Listener", port=80)
+        listener.add_targets("Target", port=80, targets=[asg])
+        listener.connections.allow_default_port_from_any_ipv4("Open to the world")
+        asg.scale_on_request_count("AModestLoad", target_requests_per_minute=60)
